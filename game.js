cf93cbfead20c843165ef84540611902a0fcb8c9
Rach Griarte <rachelgriarte@hotmail.com>
2017-10-05 14:34:31 -0400

    Finished styling 'index.html'

--- game.js
+++ game.js
@@ -1,28 +1,29 @@
-////////////////// The Contact /////////////////
-// Hides my contact information/social media 
-var close = $(
-    '<i class="material-icons" id="hide">close</i>'
-    );
+//Hide the info and social media
+$('.info').hide();
+$('.socialmedia').hide();
+$('#hide').hide();
 // If you click on the icon
 $('#contact').click(function() {
-    // It renders my contact information/social media 
-    $('.info').html(
-        // Social Media Links
-        '<a href="https://www.linkedin.com/in/rachelgriarte/"><img id=socialmedia src="images/li.jpg"></a> ' + 
-        '<a href="https://www.github.com/rachgriarte/"><img id=socialmedia src="images/gh.jpg"></a> ' +
-        '<a href="tel:+19196125115"><img id=socialmedia src="images/call.jpg"></a>' +
-        '<a href="https://www.instagram.com/rachgriarte/"><img id=socialmedia src="images/ig.jpg"></a>' +
-        '<img id=socialmedia src="images/sc.jpg">' +
-        //Want to get in touch?
-        '<p><b>Wanna get in touch or talk to me about a project? </b></p>' +
-        '<p> Just fill the form below or send an email to rachgriarte@griarte.com and ~lets talk! </p>' +
-        '<p> <a href="mailto:rachgriarte@griarte.com">rachgriarte@griarte.com</a> </p>'
-    );
+     //It appends info and message form
+    $('.info').show();
+     // It renders my social media icon
+     $('.socialmedia').show();
     // After hitting the button, hide the icon
     $('.footer').hide();
-    // Hides my contact information/social media 
-    $('.info').append(close);
+    // the close icon shows
+    $('#hide').show();
 });
-// If someone hits the close icon, it hides the contact information
-
-////////////////// Email Me ///////////////////////
+// If someone hits the close icon, it hides the information
+$('#hide').click(function() {
+    //It hdies the info and message form
+   $('.info').hide();
+    // It hides my social media icon
+    $('.socialmedia').hide();
+   // After hitting the button, show the icon
+   $('.footer').show();
+//    Hide the close icon
+   $('#hide').hide();
+});
+// Email/Message form
+$('#textarea1').val('New Text');
+$('#textarea1').trigger('autoresize');
