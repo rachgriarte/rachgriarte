cca668c917835b247e9c19253506329a6327c453
Rach Griarte <rachelgriarte@hotmail.com>
2017-11-09 20:56:16 -0500

    fixed bugs

--- apps/route/htmlroutes.js
+++ apps/route/htmlroutes.js
@@ -4,16 +4,16 @@ module.exports = function (app) {
 
     //sends the user the index.html file 
     app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname + '/index.html'));
+        res.sendFile(path.join(__dirname + '/../index.html'));
     });
 
     //sends the user the about.html file 
     app.get('/about', function (req, res) {
-        res.sendFile(path.join(__dirname + '/about.html'));
+        res.sendFile(path.join(__dirname + '/../about.html'));
     });
 
     //sends the user the projects.html file 
     app.get('/projects', function (req, res) {
-        res.sendFile(path.join(__dirname + '/projects.html'));
+        res.sendFile(path.join(__dirname + '/../projects.html'));
     });
 };
