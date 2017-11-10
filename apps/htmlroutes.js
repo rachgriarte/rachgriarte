f2cff5cdacc0539056db71a8c7678a892e087e58
Rach Griarte <rachelgriarte@hotmail.com>
2017-11-09 20:27:22 -0500

    direct link

--- apps/htmlroutes.js
+++ apps/htmlroutes.js
@@ -8,12 +8,12 @@ module.exports = function (app) {
     });
 
     //sends the user the about.html file 
-    app.get('/story', function (req, res) {
+    app.get('/about', function (req, res) {
         res.sendFile(path.join(__dirname + '/about.html'));
     });
 
     //sends the user the projects.html file 
-    app.get('/contact', function (req, res) {
+    app.get('/projects', function (req, res) {
         res.sendFile(path.join(__dirname + '/projects.html'));
     });
 };
